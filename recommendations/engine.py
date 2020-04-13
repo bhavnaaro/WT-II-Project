e95f1b7e6187dd34417b5debfecb020f871cf5fa
Dev Bhartra <devbhartra183@gmail.com>
2020-04-14 00:21:05 +0530

    Better readability

--- recommendations/engine.py
+++ recommendations/engine.py
@@ -1,17 +1,29 @@
+# for reading and manipulation of our datasets
 import pandas as pd
+# for mongo and python interaction
+from pymongo import MongoClient
 
+# parameter here is id of the course that the customer buys
 def get_recommendations(id):
     orders = pd.read_csv("OrderProduct.csv")
+
+    # only retrieving the unique orders who's id match input parameter id
     orders_for_product = orders[orders.product_id == id].order_id.unique()
+
+    # getting only those rows from the dataset that have the right id
     relevant_orders = orders[orders.order_id.isin(orders_for_product)]
     accompanying_products_by_order = relevant_orders[relevant_orders.product_id != id]
+
+    # number of of instances in which these additional courses were purchased. Basically, getting the accompanying courses:
     num_instance_by_accompanying_product = accompanying_products_by_order.groupby(
         "product_id")["product_id"].count().reset_index(name="instances")
+
+    # making a new dataframe that takes the top three results by frequency
     num_orders_for_product = orders_for_product.size
     product_instances = pd.DataFrame(num_instance_by_accompanying_product)
-    product_instances["frequency"] = product_instances["instances"] / \
-        num_orders_for_product
+    product_instances["frequency"] = product_instances["instances"]/num_orders_for_product
 
+    # final recommendation dataframe
     recommended_products = pd.DataFrame(
         product_instances.sort_values("frequency", ascending=False).head(3))
 
