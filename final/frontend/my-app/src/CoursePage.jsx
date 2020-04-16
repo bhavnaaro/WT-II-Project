cf77a0e07c3d5990586a6813b182dcae49f861f8
Bhavna Arora <47357937+bhavnaaro@users.noreply.github.com>
2020-04-16 17:00:18 +0530

    Update CoursePage.jsx

--- final/frontend/my-app/src/CoursePage.jsx
+++ final/frontend/my-app/src/CoursePage.jsx
@@ -7,54 +7,55 @@ class CoursePage extends Component {
 
   constructor(props) {
 		super(props);
-    this.state = { courseData: {}, enrollStatus: "unenrolled" };
-    this.handleEnroll = this.handleEnroll.bind(this);
+    this.state = { courseData: {}};
+    //, enrollStatus: "unenrolled" };
+    //this.handleEnroll = this.handleEnroll.bind(this);
 	}
 
 	componentDidMount() {
     var cname = this.props.match.params.coursename;
 
-		// axios.get('http://localhost:5000/')
-		// 		.then(res => {
-		// 				this.setState({ courseData: res.data });
-		// 		})
-		// 		.catch(function (error) {
-		// 				console.log(error);
-    // 		});
+		axios.get('http://localhost:5000/course/'+cname)
+				.then(res => {
+						this.setState({ courseData: res.data });
+				})
+				.catch(function (error) {
+						console.log(error);
+    		});
     
-    this.setState({ 
-      courseData: { 
-        'img': cname + '.jpg',
-        'courseName': cname,
-        'courseDesc': "this is the description for the "+cname+" it is very useful course"
-      },
-      enrollStatus: this.state.enrollStatus
-    });
+    // this.setState({ 
+    //   courseData: { 
+    //     'img': cname + '.jpg',
+    //     'courseName': cname,
+    //     'courseDesc': "this is the description for the "+cname+" it is very useful course"
+    //   },
+    //   enrollStatus: this.state.enrollStatus
+    // });
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    if (nextState.enrollStatus==="enrolling") {
-      // axios.get('http://localhost:5000/')
-      // 		.then(res => {
-      // 				this.setState({
-      //          courseData: this.state.courseData,
-      //          enrollStatus: "enrolled"
-      //        });
-      // 		})
-      // 		.catch(function (error) {
-      // 				console.log(error);
-      // 		});
-    }
-    return true;
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   if (nextState.enrollStatus==="enrolling") {
+  //     // axios.get('http://localhost:5000/')
+  //     // 		.then(res => {
+  //     // 				this.setState({
+  //     //          courseData: this.state.courseData,
+  //     //          enrollStatus: "enrolled"
+  //     //        });
+  //     // 		})
+  //     // 		.catch(function (error) {
+  //     // 				console.log(error);
+  //     // 		});
+  //   }
+  //   return true;
+  // }
   
-  handleEnroll(e) {
-    e.preventDefault();
-    this.setState({
-      courseData: this.state.courseData,
-      enrollStatus: "enrolling"
-    });
-  }
+  // handleEnroll(e) {
+  //   e.preventDefault();
+  //   this.setState({
+  //     courseData: this.state.courseData,
+  //     enrollStatus: "enrolling"
+  //   });
+  // }
 
   render() {
     var cdata = this.state.courseData
@@ -62,17 +63,12 @@ class CoursePage extends Component {
       return(
       <Container>
         <Jumbotron>
-          <img style={{display: "block", marginLeft: "auto", marginRight: "auto"}} src={require('./' + cdata['img'])} alt="" />
+          <img style={{display: "block", marginLeft: "auto", marginRight: "auto"}} src={require('./' + cdata['courseName']+".jpg")} alt="" />
         </Jumbotron>
         <Col xs={12} sm={8} smoffset={2}>
             <h2>{cdata['courseName']}</h2>
-            <p>{cdata['courseDesc']}</p>
-            <Button size="lg" block variant="dark" 
-              disabled={this.state.enrollStatus!=="unenrolled"} 
-              onclick={(this.state.enrollStatus==="unenrolled") ? this.handleEnroll : null} 
-            >
-              {(this.state.enrollStatus==="unenrolled") ? "Enroll" : ((this.state.enrollStatus==="enrolling") ? "Enrolling" : "Enrolled")}
-            </Button>
+            <p>{cdata['desc']}</p>
+            
         </Col>
 			</Container>);
     }
