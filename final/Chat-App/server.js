8f12eb9c2863c0896bccf283364a54a26223823a
Bhavna Arora <47357937+bhavnaaro@users.noreply.github.com>
2020-04-16 16:28:03 +0530

    Update server.js

--- final/Chat-App/server.js
+++ final/Chat-App/server.js
@@ -3,7 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require("socket.io")(http);
 
-var port = 3000;
+var port = 8080;
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/');
 });
