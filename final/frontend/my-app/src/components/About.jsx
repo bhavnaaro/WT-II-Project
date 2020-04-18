213d37a54c761350b610fd78ddcb108e98070c36
raunaks42 <raunaks42@gmail.com>
2020-04-18 09:06:37 +0530

    Final

--- final/frontend/my-app/src/components/About.jsx
+++ final/frontend/my-app/src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Jumbotron, Container, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 
 import './About.css';
 import h2 from '../images/h2.jpg';
@@ -7,14 +7,19 @@ import h2 from '../images/h2.jpg';
 function About(props) {
 	return (
 		<Container>
-			<Jumbotron style={{padding:0, backgroundColor: "#141415"}}>
-				<img style={{display: "block", marginLeft: "auto", marginRight: "auto"}} src={h2} alt="" />
-			</Jumbotron>
-			<Col xs={14} sm={10} smoffset={2}>
-				<Image src={require('../images/'+props.location.imgSrc)} className="about-profile-pic" rounded />
-				<h3>{props.location.name}</h3>
-				<p>{props.location.desc}</p>
-			</Col>
+			<Row className="justify-content-md-center">
+				<Col xs={14} sm={10}>
+					<img style={{display: "block", marginLeft: "auto", marginRight: "auto"}} src={h2} alt="" />
+				</Col>
+			</Row>
+			<br/><br/><br/>
+			<Row className="justify-content-md-center">
+				<Col xs={14} sm={10}>
+					<Image src={require('../images/'+props.location.imgSrc)} className="about-profile-pic" rounded />
+					<h3>{props.location.name}</h3>
+					<p>{props.location.desc}</p>
+				</Col>
+			</Row>
 			</Container>
 	);
 }
